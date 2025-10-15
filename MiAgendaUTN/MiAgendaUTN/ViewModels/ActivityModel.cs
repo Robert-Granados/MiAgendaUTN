dc579ba884d7a177f1d7e1b8cd1972f673121f53
IsaacOrtega26 <isaacd260624@gmail.com>
2025-10-14 18:58:49 -0600

    creacion de validacion

---
+++ MiAgendaUTN/MiAgendaUTN/ViewModels/ActivityModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiAgendaUTN.ViewModels
+{
+    public class ActivityModel
+    {
+        [Required(ErrorMessage = "El título es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El título no puede tener más de 100 caracteres.")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
+        [StringLength(250, ErrorMessage = "La descripción no puede tener más de 250 caracteres.")]
+        public string Descripcion { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La fecha es obligatoria.")]
+        [FechaNoPasada]
+        public DateTime Fecha { get; set; } = DateTime.Today;
+
+
+
+        [Required(ErrorMessage = "La categoría es obligatoria.")]
+        public int Categoria { get; set; }
+
+        public class FechaNoPasadaAttribute : ValidationAttribute
+        {
+            public FechaNoPasadaAttribute()
+            {
+                ErrorMessage = "La fecha no puede ser anterior a hoy.";
+            }
+            public override bool IsValid(object? value)
+            {
+                if (value is DateTime fecha)
+                {
+                    return fecha >= DateTime.Today;
+                }
+                return false;
+            }
+        }
+    }
+}
